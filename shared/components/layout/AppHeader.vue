c1bd81912cbbd79b527f355cd42726fff5af5d84
sheon-j <jsh7704@naver.com>
2025-06-15 17:44:08 +0900

    style: logo 추가

--- shared/components/layout/AppHeader.vue
+++ shared/components/layout/AppHeader.vue
@@ -7,18 +7,13 @@
     style="backdrop-filter: blur(30px)"
   >
     <v-app-bar-title class="text-center">
-      <v-avatar color="inverse" size="x-small" rounded="lg" class="mr-1">
-        <v-icon size="x-small" :icon="mdiNewspaperVariantOutline" />
-      </v-avatar>
-      <span v-text="APP_NAME" />
+      <v-img :src="logo" height="32" />
     </v-app-bar-title>
   </v-app-bar>
 </template>
 
 <script setup lang="ts">
-import { mdiNewspaperVariantOutline } from '@mdi/js'
-
-const APP_NAME = 'Awesome News'
+import logo from '@/assets/logo.svg'
 </script>
 
 <style scoped></style>
